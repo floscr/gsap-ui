761ea23b7b08bd3688f747edb28b713caab72c5b
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-04-04 17:06:31 +0200

    Eslint fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,58 +1,58 @@
-import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import ReplacePlugin from 'replace-bundle-webpack-plugin';
-import OfflinePlugin from 'offline-plugin';
-import path from 'path';
-import V8LazyParseWebpackPlugin from 'v8-lazy-parse-webpack-plugin';
-const ENV = process.env.NODE_ENV || 'development';
+import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import autoprefixer from 'autoprefixer'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import ReplacePlugin from 'replace-bundle-webpack-plugin'
+import OfflinePlugin from 'offline-plugin'
+import path from 'path'
+import V8LazyParseWebpackPlugin from 'v8-lazy-parse-webpack-plugin'
+const ENV = process.env.NODE_ENV || 'development'
 
-const CSS_MAPS = ENV!=='production';
+const CSS_MAPS = ENV !== 'production'
 
 module.exports = {
-	context: path.resolve(__dirname, "src"),
-	entry: './index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
 
-	output: {
-		path: path.resolve(__dirname, "build"),
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
 
-	resolve: {
-		extensions: ['.jsx', '.js', '.json', '.less'],
-		modules: [
-			path.resolve(__dirname, "src/lib"),
-			path.resolve(__dirname, "node_modules"),
-			'node_modules'
-		],
-		alias: {
-			components: path.resolve(__dirname, "src/components"),    // used for tests
-			style: path.resolve(__dirname, "src/style"),
-			'react': 'preact-compat',
-			'react-dom': 'preact-compat'
-		}
-	},
+  resolve: {
+    extensions: ['.jsx', '.js', '.json', '.less'],
+    modules: [
+      path.resolve(__dirname, 'src/lib'),
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),    // used for tests
+      style: path.resolve(__dirname, 'src/style'),
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: path.resolve(__dirname, 'src'),
-				enforce: 'pre',
-				use: 'source-map-loader'
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			},
-			{
-				// Transform our own .(less|css) files with PostCSS and CSS-modules
-				test: /\.(less|css)$/,
-				include: [path.resolve(__dirname, 'src/components')],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: path.resolve(__dirname, 'src'),
+        enforce: 'pre',
+        use: 'source-map-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        // Transform our own .(less|css) files with PostCSS and CSS-modules
+        test: /\.(less|css)$/,
+        include: [path.resolve(__dirname, 'src/components')],
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
@@ -64,7 +64,7 @@ module.exports = {
               loader: `postcss-loader`,
               options: {
                 plugins: () => {
-                  autoprefixer({ browsers: [ 'last 2 versions' ] });
+                  autoprefixer({ browsers: [ 'last 2 versions' ] })
                 }
               }
             },
@@ -74,10 +74,10 @@ module.exports = {
             }
           ]
         })
-			},
-			{
-				test: /\.(less|css)$/,
-				exclude: [path.resolve(__dirname, 'src/components')],
+      },
+      {
+        test: /\.(less|css)$/,
+        exclude: [path.resolve(__dirname, 'src/components')],
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
@@ -89,7 +89,7 @@ module.exports = {
               loader: `postcss-loader`,
               options: {
                 plugins: () => {
-                  autoprefixer({ browsers: [ 'last 2 versions' ] });
+                  autoprefixer({ browsers: [ 'last 2 versions' ] })
                 }
               }
             },
@@ -99,109 +99,109 @@ module.exports = {
             }
           ]
         })
-			},
-			{
-				test: /\.json$/,
-				use: 'json-loader'
-			},
-			{
-				test: /\.(xml|html|txt|md)$/,
-				use: 'raw-loader'
-			},
-			{
-				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-				use: ENV==='production' ? 'file-loader' : 'url-loader'
-			}
-		]
-	},
-	plugins: ([
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.(xml|html|txt|md)$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+        use: ENV === 'production' ? 'file-loader' : 'url-loader'
+      }
+    ]
+  },
+  plugins: ([
     new webpack.NoEmitOnErrorsPlugin(),
     new ExtractTextPlugin({
       filename: 'style.css',
       allChunks: true,
       disable: ENV !== 'production'
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(ENV)
-		}),
-		new HtmlWebpackPlugin({
-			template: './index.ejs',
-			minify: { collapseWhitespace: true }
-		}),
-		new CopyWebpackPlugin([
-			{ from: './manifest.json', to: './' },
-			{ from: './favicon.ico', to: './' }
-		])
-	]).concat(ENV==='production' ? [
-		new V8LazyParseWebpackPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			output: {
-				comments: false
-			},
-			compress: {
-				conditionals: true,
-				unused: true,
-				comparisons: true,
-				sequences: true,
-				dead_code: true,
-				evaluate: true,
-				if_return: true,
-				join_vars: true,
-				negate_iife: false,
-				warnings: false
-			}
-		}),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.ejs',
+      minify: { collapseWhitespace: true }
+    }),
+    new CopyWebpackPlugin([
+      { from: './manifest.json', to: './' },
+      { from: './favicon.ico', to: './' }
+    ])
+  ]).concat(ENV === 'production' ? [
+    new V8LazyParseWebpackPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        negate_iife: false,
+        warnings: false
+      }
+    }),
 
-		// strip out babel-helper invariant checks
-		new ReplacePlugin([{
-			// this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
-			partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
-			replacement: () => 'return;('
-		}]),
-		new OfflinePlugin({
-			relativePaths: false,
-			AppCache: false,
-			excludes: ['_redirects'],
-			ServiceWorker: {
-				events: true
-			},
-			cacheMaps: [
-				{
-					match: /.*/,
-					to: '/',
-					requestTypes: ['navigate']
-				}
-			],
-			publicPath: '/'
-		})
-	] : []),
+    // strip out babel-helper invariant checks
+    new ReplacePlugin([{
+      // this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
+      partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
+      replacement: () => 'return;('
+    }]),
+    new OfflinePlugin({
+      relativePaths: false,
+      AppCache: false,
+      excludes: ['_redirects'],
+      ServiceWorker: {
+        events: true
+      },
+      cacheMaps: [
+        {
+          match: /.*/,
+          to: '/',
+          requestTypes: ['navigate']
+        }
+      ],
+      publicPath: '/'
+    })
+  ] : []),
 
-	stats: { colors: true },
+  stats: { colors: true },
 
-	node: {
-		global: true,
-		process: false,
-		Buffer: false,
-		__filename: false,
-		__dirname: false,
-		setImmediate: false
-	},
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
+  },
 
-	devtool: ENV==='production' ? 'source-map' : 'cheap-module-eval-source-map',
+  devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
 
-	devServer: {
-		port: process.env.PORT || 8080,
-		host: 'localhost',
-		publicPath: '/',
-		contentBase: './src',
-		historyApiFallback: true,
-		open: true,
-		proxy: {
-			// OPTIONAL: proxy configuration:
-			// '/optional-prefix/**': { // path pattern to rewrite
-			//   target: 'http://target-host.com',
-			//   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
-			// }
-		}
-	}
-};
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: 'localhost',
+    publicPath: '/',
+    contentBase: './src',
+    historyApiFallback: true,
+    open: true,
+    proxy: {
+      // OPTIONAL: proxy configuration:
+      // '/optional-prefix/**': { // path pattern to rewrite
+      //   target: 'http://target-host.com',
+      //   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
+      // }
+    }
+  }
+}
